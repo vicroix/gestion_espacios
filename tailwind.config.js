71f54d24791a71a1ca74b226536d533840231f1c
VictorMedac <vbj0003@alu.medac.es>
2025-03-27 00:34:20 +0100

    tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./*.html",   // index.html
+    "./js/*.js",
+    "./css/*.css"     // script.js
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
