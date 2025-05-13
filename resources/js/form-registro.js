4e24a7c30f14757e6ad01a3fb247e83e163bd44b
andresfvieira <afv0015@alu.medac.es>
2025-05-13 13:30:16 +0200

    arreglar iconos formregistro

--- resources/js/form-registro.js
+++ resources/js/form-registro.js
@@ -2,12 +2,12 @@
 //Función para que no permita dejar campos vacios
 document.getElementById('formRegistro').addEventListener('submit', function (event) {
 
-    const nombre = document.getElementById('nombre').value.trim;
+    const nombre = document.getElementById('nombre').value.trim();
     const apellidos = document.getElementById('apellidos').value;
-    const usuario = document.getElementById('usuario').value.trim;
-    const password = document.getElementById('password').value.trim;
-    const email = document.getElementById('email').value.trim;
-    const telefono = document.getElementById('telefono').value.trim;
+    const usuario = document.getElementById('usuario').value.trim();
+    const password = document.getElementById('password').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const telefono = document.getElementById('telefono').value.trim();
 
     if (nombre === '') {
         alert('El nombre es obligatorio.');
@@ -44,8 +44,8 @@ document.getElementById('apellidos').addEventListener('input', function () {
 document.getElementById('telefono').addEventListener('input', function () {
     this.value = this.value.replace(/[^0-9]/g, '');
 })
-//Validación correo
 
+//Validación correo
 document.getElementById('email').addEventListener('submit', function () {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!emailRegex.test(email)) {
@@ -55,12 +55,13 @@ document.getElementById('email').addEventListener('submit', function () {
 
 //Validación contraseña
 document.getElementById('formRegistro').addEventListener('submit', function (event) {
-    const password = getElementById('password').value.trim;
+    const password = document.getElementById('password').value.trim();
     const cumpleRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{6,}$/;
     const mensaje = document.getElementById("mensaje");
     if (cumpleRegex.test(password)) {
         mensaje.style.color = "green";
         mensaje.textContent = "Contraseña válida.";
+        
     } else {
         mensaje.style.color = "red";
         mensaje.textContent = "La contraseña debe incluir 1 mayúscula, 1 minúscula, 1 número y al menos 6 carácteres";
