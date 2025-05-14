0de6cc543228699b136e7fc843ed1ae26be754a3
VictorMedac <vbj0003@alu.medac.es>
2025-05-14 17:51:16 +0200

    Cambio povisional y responsive de editar reservas

--- resources/views/editar-reservas.blade.php
+++ resources/views/editar-reservas.blade.php
@@ -13,18 +13,20 @@
     </div>
     <form method="POST" action="{{ route('actualizar-reserva', ['id' => $reserva->idreservas]) }}" class="flex flex-col md:flex md:flex-row items-center gap-3">
         @csrf
+        <input type="hidden" name="id_espacio" value="{{ $reserva->id_espacio }}">
         <label>Fecha:</label>
         <input type="date" id="fecha" name="fecha" min="{{ date('Y-m-d') }}" value="{{ $reserva->fecha }}" required class="inputs-text border border-[--color-secundario]">
-
-        <label>Hora inicio:</label>
-        <input type="time" id="horaInicio" name="hora" value="{{ $reserva->hora }}" required class="inputs-text border border-[--color-secundario]">
-
-        <label>Hora fin:</label>
-        <input type="time" id="horaFin" name="hora_fin" value="{{ $reserva->hora_fin }}" required class="inputs-text border border-[--color-secundario]">
-
-        <input type="hidden" name="id_espacio" value="{{ $reserva->id_espacio }}">
-
-        <button type="submit" class="button-primary-auto">Actualizar Reserva</button>
+        <div class="flex gap-3">
+            <div class="flex flex-col">
+                <label>Hora inicio:</label>
+                <input type="time" id="horaInicio" name="hora" value="{{ $reserva->hora }}" required class="inputs-text border border-[--color-secundario]">
+            </div>
+            <div class="flex flex-col">
+                <label>Hora fin:</label>
+                <input type="time" id="horaFin" name="hora_fin" value="{{ $reserva->hora_fin }}" required class="inputs-text border border-[--color-secundario]">
+            </div>
+        </div>
+        <button type="submit" class="button-primary-auto mt-4">Actualizar</button>
     </form>
     @if (session('error'))
     <p class="text-red-500">{{ session('error') }}</p>
