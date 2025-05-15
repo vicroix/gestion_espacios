c52a1971162e97faa886a22b130c1ebc7b413566
Borja <frb0005@alu.medac.es>
2025-05-15 12:56:00 +0200

    Añadir salas solo para admin

--- resources/views/nuevas-reservas.blade.php
+++ resources/views/nuevas-reservas.blade.php
@@ -170,19 +170,22 @@
                         </div>
                     </form>
                 </aside>
-                <!-- Sidebar de añadir salas -->
-                <aside class="w-64 px-2 shadow-md">
-                    <a href="{{ route('gestion-salas') }}" class="w-full">
-                    <h2 id="h2" class="mt-2 mb-2">
-                        <!--Icono de añadir-->
-                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24">
-                            <path fill="#000000"
-                                d="M17 13h-4v4h-2v-4H7v-2h4V7h2v4h4m-5-9A10 10 0 0 0 2 12a10 10 0 0 0 10 10a10 10 0 0 0 10-10A10 10 0 0 0 12 2" />
-                        </svg>
-                        Añadir sala
-                    </h2>
-                    </a>
-                </aside>
+                @if (session('id_rol') === 1)
+                    <!-- Sidebar de añadir salas -->
+                    <aside class="w-64 px-2 shadow-md">
+                        <a href="{{ route('gestion-salas') }}" class="w-full">
+                            <h2 id="h2" class="mt-2 mb-2">
+                                <!--Icono de añadir-->
+                                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25"
+                                    viewBox="0 0 24 24">
+                                    <path fill="#000000"
+                                        d="M17 13h-4v4h-2v-4H7v-2h4V7h2v4h4m-5-9A10 10 0 0 0 2 12a10 10 0 0 0 10 10a10 10 0 0 0 10-10A10 10 0 0 0 12 2" />
+                                </svg>
+                                Añadir sala
+                            </h2>
+                        </a>
+                    </aside>
+                @endif
             </div>
             <!-- Resultados -->
             <section
