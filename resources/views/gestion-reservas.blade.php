3ab3979480f9837e35ec58c7a0b887a8e042dbe5
VictorMedac <vbj0003@alu.medac.es>
2025-05-16 18:49:06 +0200

    Boton limpiar filtros

--- resources/views/gestion-reservas.blade.php
+++ resources/views/gestion-reservas.blade.php
@@ -32,22 +32,39 @@
     <div x-data="{ openHora: false,openFecha:false ,openCiudades: false, openTipo: false, openFiltros: false, openAforo: false }" class="flex flex-col items-center min-w-[320px] gap-1 lg:items-start lg:flex-row">
         <!-- Sidebar de filtros -->
         <aside class="w-64 px-2 shadow-md">
-            <h2 id="h2" @click="openFiltros = !openFiltros" class="mt-2">
-                <!--Icono de filtros-->
-                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24">
-                    <path fill="currentColor"
-                        d="M9 5a1 1 0 1 0 0 2a1 1 0 0 0 0-2M6.17 5a3.001 3.001 0 0 1 5.66 0H19a1 1 0 1 1 0 2h-7.17a3.001 3.001 0 0 1-5.66 0H5a1 1 0 0 1 0-2zM15 11a1 1 0 1 0 0 2a1 1 0 0 0 0-2m-2.83 0a3.001 3.001 0 0 1 5.66 0H19a1 1 0 1 1 0 2h-1.17a3.001 3.001 0 0 1-5.66 0H5a1 1 0 1 1 0-2zM9 17a1 1 0 1 0 0 2a1 1 0 0 0 0-2m-2.83 0a3.001 3.001 0 0 1 5.66 0H19a1 1 0 1 1 0 2h-7.17a3.001 3.001 0 0 1-5.66 0H5a1 1 0 1 1 0-2z" />
-                </svg>
-                Filtrar reservas
-            </h2>
+            <div class="flex justify-between p-2">
+                <h2 id="h2" @click="openFiltros = !openFiltros">
+                    <!--Icono de filtros-->
+                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24">
+                        <path fill="currentColor"
+                            d="M9 5a1 1 0 1 0 0 2a1 1 0 0 0 0-2M6.17 5a3.001 3.001 0 0 1 5.66 0H19a1 1 0 1 1 0 2h-7.17a3.001 3.001 0 0 1-5.66 0H5a1 1 0 0 1 0-2zM15 11a1 1 0 1 0 0 2a1 1 0 0 0 0-2m-2.83 0a3.001 3.001 0 0 1 5.66 0H19a1 1 0 1 1 0 2h-1.17a3.001 3.001 0 0 1-5.66 0H5a1 1 0 1 1 0-2zM9 17a1 1 0 1 0 0 2a1 1 0 0 0 0-2m-2.83 0a3.001 3.001 0 0 1 5.66 0H19a1 1 0 1 1 0 2h-7.17a3.001 3.001 0 0 1-5.66 0H5a1 1 0 1 1 0-2z" />
+                    </svg>
+                    Filtrar reservas
+                </h2>
+            </div>
             <form method="GET" action="{{ route('filtro-reservas') }}" class="space-y-0 mb-2 pl-1">
                 <!-- Fecha -->
-                <div class="flex flex-col w-[150px]">
-                    <input type="date" name="fecha" class="mr-2 my-1" value="{{ old('fecha') }}">
+                <div class="flex justify-between relative">
+                    <input type="date" name="fecha" class="mr-2 my-1 p-1 w-[160px]" value="{{ old('fecha') }}">
+                    <!-- Botón para resetear filtro -->
+                    <button type="reset" class="absolute right-0 -translate-y-[54px] w-[30px]" title="Limpiar">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 16 16" class="mb-4 p-1 w-full items-center hover:shadow-around border-2 hover:shadow-gray-400 rounded-md">
+                            <g fill="none">
+                                <g clip-path="url(#IconifyId196d9d8a7b2e9fd2d1)">
+                                    <path fill="currentColor" fill-rule="evenodd" d="M11.995.667a.75.75 0 1 0-1.49.166L11.19 7h-.867c-1.64 0-2.896 1.449-3.197 3.06a12.6 12.6 0 0 1-1.2 3.44C5.434 14.448 5 15 5 15h8.5s2.08-1.734 2.488-5.49C16.14 8.094 14.91 7 13.488 7H12.7zM3.75 2.5a.75.75 0 1 0 0 1.5h4.5a.75.75 0 0 0 0-1.5zM.75 6a.75.75 0 1 0 0 1.5h5.5a.75.75 0 0 0 0-1.5zM1 10.25a.75.75 0 0 1 .75-.75h2.5a.75.75 0 0 1 0 1.5h-2.5a.75.75 0 0 1-.75-.75m6.593 3.25c.393-.866.78-1.94 1.008-3.165C8.819 9.167 9.646 8.5 10.322 8.5h3.167c.332 0 .618.13.797.303a.63.63 0 0 1 .21.545c-.175 1.622-.708 2.779-1.173 3.514a6 6 0 0 1-.461.638h-.999c.539-.706.887-1.728.887-2.75H12c-.351 1.229-1.072 2.088-2.162 2.75z" clip-rule="evenodd" />
+                                </g>
+                                <defs>
+                                    <clipPath id="IconifyId196d9d8a7b2e9fd2d1">
+                                        <path fill="currentColor" d="M0 0h16v16H0z" />
+                                    </clipPath>
+                                </defs>
+                            </g>
+                        </svg>
+                    </button>
                 </div>
                 <!-- Hora -->
-                <div class="flex flex-col my-3 w-[90px]">
-                    <input type="time" name="hora" class="mr-2 my-1" value="{{ old('hora') }}">
+                <div class="flex flex-col my-3 w-[100px]">
+                    <input type="time" name="hora" class="mr-2 my-1 p-1" value="{{ old('hora') }}">
                 </div>
                 <!-- + Filtros -->
                 <div class="mt-4">
