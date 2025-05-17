2c69506b43b4fb35d2833496ee8fe4d46b63c7af
VictorMedac <vbj0003@alu.medac.es>
2025-05-18 00:46:30 +0200

    Diseño navbar eliminación div sobrantes

--- resources/views/layouts/navbars/navbars-menu/navbars-menu-admin/navbar-menu-escritorio-admin.blade.php
+++ resources/views/layouts/navbars/navbars-menu/navbars-menu-admin/navbar-menu-escritorio-admin.blade.php
@@ -1,16 +1,10 @@
 <div class="flex border-2 border-[--color-primario] rounded-3xl h-[40px] items-center">
-    <!-- Reservas -->
-    <div class="relative group">
-        <a href="{{ url('gestion-reservas') }}" class="px-3 py-1 hover:bg-[--color-primario] border group-hover:bg-[--color-primario] group-hover:text-[--color-general] border-[--color-general] hover:text-[--color-general] rounded-l-3xl transition-all duration-300 ease-out">
-            Reservas
-        </a>
-    </div>
-    <!-- Salas -->
-    <div class="relative group">
-        <a href="{{ url('gestion-salas') }}" class="px-3 py-1 hover:bg-[--color-primario] border border-[--color-general] group-hover:bg-[--color-primario] group-hover:text-[--color-general] hover:text-[--color-general] transition-all duration-300 ease-out">
-            Gestión Salas
-        </a>
-    </div>
+    <a href="{{ url('gestion-reservas') }}" class="px-3 py-1 hover:bg-[--color-primario] border group-hover:bg-[--color-primario] group-hover:text-[--color-general] border-[--color-general] hover:text-[--color-general] rounded-l-3xl transition-all duration-300 ease-out">
+        Reservas
+    </a>
+    <a href="{{ url('gestion-salas') }}" class="px-3 py-1 hover:bg-[--color-primario] border border-[--color-general] group-hover:bg-[--color-primario] group-hover:text-[--color-general] hover:text-[--color-general] transition-all duration-300 ease-out">
+        Gestión Salas
+    </a>
     <a href="{{ url('proximos-eventos') }}" class="hover:bg-[--color-primario] px-3 border-2 w-[180px] border-[--color-general] hover:text-[--color-general] p-1 transition-all duration-300 ease-out">Próximos eventos</a>
     <a href="{{ url('faq') }}" class="hover:bg-[--color-primario] border px-3 border-[--color-general] hover:text-[--color-general] rounded-r-3xl p-1 transition-all duration-300 ease-out">FAQ</a>
 </div>
