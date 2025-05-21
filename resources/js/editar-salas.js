3d6aff3036012a08f75e0aa095c99e3899505e1e
andresfvieira <afv0015@alu.medac.es>
2025-05-21 10:10:54 +0200

    editar-salas.php errores form

--- resources/js/editar-salas.js
+++ resources/js/editar-salas.js
@@ -1,12 +1,8 @@
-//Validacion form
-window.cargarFormularioEditarSalas=function(){
+document.addEventListener('DOMContentLoaded', () => {
+    const formulario = document.getElementById('formGestionSalas');
 
-const form = document.getElementById("formGestionSalas");
- if (!form) return;
-
- form.addEventListener("submit", function(event){
-//Toma de valores
-     const nombre_teatro = document.getElementById("nombre_teatro").value;
+    formulario.addEventListener("submit", function (event) {
+        const nombre_teatro = document.getElementById("nombre_teatro").value;
         const nombre_sala = document.getElementById("nombre_sala").value;
         const telefono = document.getElementById("telefono").value;
         const localidad = document.getElementById("localidad").value;
@@ -16,17 +12,16 @@ const form = document.getElementById("formGestionSalas");
         const tipo_sala = document.getElementById("tipo_sala").value;
         const aforo = document.getElementById("aforo").value;
         const equipamiento = document.getElementById("equipamiento").value;
-//Impedir que se puedan dejar los campos vacios
-        if (nombre_teatro === "" || nombre_sala === "" || telefono === "" || 
+        //Impedir que se puedan dejar los campos vacios
+        if (nombre_teatro === "" || nombre_sala === "" || telefono === "" ||
             localidad === "" || codigo_postal === "" || direccion === "" ||
             email === "" || tipo_sala === "" || aforo === ""
         ) {
             alert("El campo es obligatorio.");
             event.preventDefault(); // Detiene el envío
         }
-       
-    });
-    //Validación para no permitir letras y limitar el numero de caracteres
+    })
+ //Validación para no permitir letras y limitar el numero de caracteres
     const inputCodigoPostal = document.getElementById("codigo_postal");
     inputCodigoPostal.addEventListener("input", function () {
         if (this.value.length > 5) {
@@ -41,5 +36,5 @@ const form = document.getElementById("formGestionSalas");
         .addEventListener("input", function () {
             this.value = this.value.replace(/[^0-9]/g, "").slice(0, 5);
         });
-};
 
+})
