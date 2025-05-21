01a8fa07fd2a6bfe87050fb1b402359b3d049261
andresfvieira <afv0015@alu.medac.es>
2025-05-21 10:26:43 +0200

    Revert "editar-salas.php errores form"
    This reverts commit 3d6aff3036012a08f75e0aa095c99e3899505e1e.

--- resources/js/editar-salas.js
+++ resources/js/editar-salas.js
@@ -1,8 +1,12 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const formulario = document.getElementById('formGestionSalas');
+//Validacion form
+window.cargarFormularioEditarSalas=function(){
 
-    formulario.addEventListener("submit", function (event) {
-        const nombre_teatro = document.getElementById("nombre_teatro").value;
+const form = document.getElementById("formGestionSalas");
+ if (!form) return;
+
+ form.addEventListener("submit", function(event){
+//Toma de valores
+     const nombre_teatro = document.getElementById("nombre_teatro").value;
         const nombre_sala = document.getElementById("nombre_sala").value;
         const telefono = document.getElementById("telefono").value;
         const localidad = document.getElementById("localidad").value;
@@ -12,16 +16,17 @@ document.addEventListener('DOMContentLoaded', () => {
         const tipo_sala = document.getElementById("tipo_sala").value;
         const aforo = document.getElementById("aforo").value;
         const equipamiento = document.getElementById("equipamiento").value;
-        //Impedir que se puedan dejar los campos vacios
-        if (nombre_teatro === "" || nombre_sala === "" || telefono === "" ||
+//Impedir que se puedan dejar los campos vacios
+        if (nombre_teatro === "" || nombre_sala === "" || telefono === "" || 
             localidad === "" || codigo_postal === "" || direccion === "" ||
             email === "" || tipo_sala === "" || aforo === ""
         ) {
             alert("El campo es obligatorio.");
             event.preventDefault(); // Detiene el envío
         }
-    })
- //Validación para no permitir letras y limitar el numero de caracteres
+       
+    });
+    //Validación para no permitir letras y limitar el numero de caracteres
     const inputCodigoPostal = document.getElementById("codigo_postal");
     inputCodigoPostal.addEventListener("input", function () {
         if (this.value.length > 5) {
@@ -36,5 +41,5 @@ document.addEventListener('DOMContentLoaded', () => {
         .addEventListener("input", function () {
             this.value = this.value.replace(/[^0-9]/g, "").slice(0, 5);
         });
+};
 
-})
--- resources/views/editar-salas.blade.php
+++ resources/views/editar-salas.blade.php
@@ -8,70 +8,70 @@
 <link rel="icon" type="image/png" href="/img/Logo.png">
 
 @section('main')
-    <main class="ml-25 mr-25">
-        <div class="flex justify-center">
-            <div class="titulo-main w-full flex justify-center md:mx-[70px]">
-                <h3 class="mt-2 md:text-2xl flex gap-2 text-2xl">MODIFICAR ESPACIO</h3>
-            </div>
+<main class="ml-25 mr-25">
+    <div class="flex justify-center">
+        <div class="titulo-main w-full flex justify-center md:mx-[70px]">
+            <h3 class="mt-2 md:text-2xl flex gap-2 text-2xl">MODIFICAR ESPACIO</h3>
         </div>
-        <!--inputs de gestión de salas-->
-        <div class="flex justify-center w-full mt-2">
-            <form id="formGestionSalas" action="{{ route('editar-espacio', ['id' => $espacio->idespacios]) }}"
-                enctype="multipart/form-data" method="POST"
-                class="bg-[--color-general] w-[315px] sm:w-[440px] m-1 rounded-lg flex-col flex gap-2 justify-center">
-                @csrf
-                <div class="w-full flex flex-col gap-2">
-                    <!--Información básica-->
-                    <div class="mb-1 bg-gray-50 p-6 rounded-lg shadow-sm">
-                        <h2 class="text-lg mb-4">Información básica</h2>
-                        <div class="flex items-center flex-col gap-5">
-                            <!-- INPUT NOMBRE TEATRO -->
-                            <input
-                                class="inputs-text border border-gray-300 focus:outline-none invalid:focus:ring-red-400 peer w-full"
-                                type="text" value="{{ $espacio->nombre }}" name="nombre_teatro" id="nombre_teatro"
-                                placeholder="Nombre del teatro">
-                            <!-- INPUT NOMBRE SALA -->
-                            <input
-                                class="inputs-text border border-gray-300 focus:outline-none
+    </div>
+    <!--inputs de gestión de salas-->
+    <div class="flex justify-center w-full mt-2">
+        <form id="formGestionSalas" action="{{ route('editar-espacio', ['id' => $espacio->idespacios]) }}"
+            enctype="multipart/form-data" method="POST"
+            class="bg-[--color-general] lg:w-[500px] m-1 rounded-lg flex-col flex gap-2 justify-center">
+            @csrf
+            <div class="w-full flex flex-col gap-2">
+                <!--Información básica-->
+                <div class="mb-1 bg-gray-50 p-6 rounded-lg shadow-sm">
+                    <h2 class="text-lg mb-4">Información básica</h2>
+                    <div class="flex items-center flex-col gap-5">
+                        <!-- INPUT NOMBRE TEATRO -->
+                        <input
+                            class="inputs-text border border-gray-300 focus:outline-none invalid:focus:ring-red-400 peer w-full"
+                            type="text" value="{{ $espacio->nombre }}" name="nombre_teatro" id="nombre_teatro"
+                            placeholder="Nombre del teatro">
+                        <!-- INPUT NOMBRE SALA -->
+                        <input
+                            class="inputs-text border border-gray-300 focus:outline-none
                      invalid:focus:ring-red-400 peer w-full"
-                                type="text" value="{{ $espacio->nombre_sala }}" name="nombre_sala" id="nombre_sala"
-                                placeholder="Nombre Sala">
+                            type="text" value="{{ $espacio->nombre_sala }}" name="nombre_sala" id="nombre_sala"
+                            placeholder="Nombre Sala">
 
-                            <div class="grid grid-cols-2 gap-4">
-                                <!-- INPUT LOCALIDAD -->
-                                <input
-                                    class="inputs-text border border-gray-300 focus:outline-none invalid:focus:ring-red-400 peer w-full"
-                                    type="text" value="{{ $espacio->localidad }}" name="localidad" id="localidad"
-                                    placeholder="Localidad">
-                                <!-- INPUT CODIGO POSTAL -->
-                                <input
-                                    class="inputs-text border border-gray-300 focus:outline-none invalid:focus:ring-red-400 peer w-full"
-                                    type="text" value="{{ $espacio->codigopostal }}" name="codigo_postal"
-                                    id="codigo_postal" placeholder="C.P.">
-                            </div>
-                            <!-- INPUT DIRECCION -->
+                        <div class="grid grid-cols-2 gap-4">
+                            <!-- INPUT LOCALIDAD -->
                             <input
                                 class="inputs-text border border-gray-300 focus:outline-none invalid:focus:ring-red-400 peer w-full"
-                                type="text" value="{{ $espacio->direccion }}" name="direccion" id="direccion"
-                                placeholder="Dirección">
+                                type="text" value="{{ $espacio->localidad }}" name="localidad" id="localidad"
+                                placeholder="Localidad">
+                            <!-- INPUT CODIGO POSTAL -->
+                            <input
+                                class="inputs-text border border-gray-300 focus:outline-none invalid:focus:ring-red-400 peer w-full"
+                                type="text" value="{{ $espacio->codigopostal }}" name="codigo_postal"
+                                id="codigo_postal" placeholder="C.P.">
                         </div>
+                        <!-- INPUT DIRECCION -->
+                        <input
+                            class="inputs-text border border-gray-300 focus:outline-none invalid:focus:ring-red-400 peer w-full"
+                            type="text" value="{{ $espacio->direccion }}" name="direccion" id="direccion"
+                            placeholder="Dirección">
                     </div>
-                    <!-- Datos de Contacto -->
-                    <div class="mb-1 bg-gray-50 p-6 rounded-lg shadow-sm">
-                        <h2 class="text-lg mb-4">Datos de Contacto</h2>
-                        <div class="grid gap-4">
-                            <!-- INPUT TELEFONO -->
-                            <input
-                                class="inputs-text border border-gray-300 focus:outline-none
+                </div>
+                <!-- Datos de Contacto -->
+                <div class="mb-1 bg-gray-50 p-6 rounded-lg shadow-sm">
+                    <h2 class="text-lg mb-4">Datos de Contacto</h2>
+                    <div class="grid gap-4">
+                        <!-- INPUT TELEFONO -->
+                        <input
+                            class="inputs-text border border-gray-300 focus:outline-none
                             invalid:focus:ring-red-400 peer w-full"
-                                type="text" value="{{ $espacio->telefono }}" name="telefono" id="telefono"
-                                placeholder="Teléfono">
-                            <!-- INPUT EMAIL -->
-                            <input
-                                class="inputs-text border border-gray-300 focus:outline-none
+                            type="text" value="{{ $espacio->telefono }}" name="telefono" id="telefono"
+                            placeholder="Teléfono">
+                        <!-- INPUT EMAIL -->
+                        <input
+                            class="inputs-text border border-gray-300 focus:outline-none
                      invalid:focus:ring-red-400 peer w-full "
-                                type="email" value="{{ $espacio->email }}" name="email" id="email"
-                                placeholder="Correo electrónico">
+                            type="email" value="{{ $espacio->email }}" name="email" id="email"
+                            placeholder="Correo electrónico">
 
                     </div>
                 </div>
