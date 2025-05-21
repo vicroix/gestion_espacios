6fd23daee20b5a4cb8c3e510c81fa0532f58b73e
eleenadm2 <edm0005@alu.medac.es>
2025-05-21 11:48:20 +0200

    estilo faq

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -8,65 +8,71 @@
 <link rel="icon" type="image/png" href="/img/Logo.png">
 
 @section("main")
-<main class="flex flex-col mt-[50px] ml-4 mr-4 lg:w-full">
-    <div class="flex justify-center">
+<main class="mt-[50px]">
+    <div class="flex justify-center mb-5">
         <div class="titulo-main w-full flex justify-center md:mx-[70px]">
-            <h3 class="mt-10 md:text-2xl text-2xl">PREGUNTAS FRECUENTES</h3>
+            <h3 class="mt-5 md:text-2xl text-2xl">PREGUNTAS FRECUENTES</h3>
         </div>
     </div>
-    <!--preguntas-->
-    <div class="lg:flex-justify-center lg:self-center lg:w-[1280px] mt-5">
-        <details>
-            <summary>¿Qué es esta plataforma?</summary>
-            <p>Esta es una plataforma en línea que permite la gestión y reserva de salas para ensayos y funciones de
-                teatro.
-                Facilita la organización de espacios y horarios para grupos teatrales, academias y artistas
-                independientes.</p>
-        </details>
+    <div class="max-w-6xl mx-auto px-4">
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-x-8 gap-y-6">
+            <!--preguntas-->
+            <div class="flex flex-col gap-2">
+                    <details class="mb-1 bg-gray-50 p-4 rounded-lg shadow-sm cursor-pointer open:border-t-4 open:border-[#990000]">
+                        <summary>¿Qué es esta plataforma?</summary>
+                        <p class="mt-5">Esta es una plataforma en línea que permite la gestión y reserva de salas para ensayos y funciones de
+                            teatro.
+                            Facilita la organización de espacios y horarios para grupos teatrales, academias y artistas
+                            independientes.</p>
+                    </details>
+                
+                    <details class="mb-1 bg-gray-50 p-4 rounded-lg shadow-sm cursor-pointer open:border-t-4 open:border-[#990000]">
+                        <summary>¿Cómo hago una reserva?</summary>
+                        <p class="mt-5">Debes iniciar sesión, seleccionar la sala deseada, elegir la fecha y horario disponible, y confirmar la
+                            reserva.</p>
+                    </details>
+               
 
-        <details>
-            <summary>¿Cómo hago una reserva?</summary>
-            <p>Debes iniciar sesión, seleccionar la sala deseada, elegir la fecha y horario disponible, y confirmar la
-                reserva.</p>
-        </details>
-
-        <details>
-            <summary>¿Hay costos asociados a la reserva de salas?</summary>
-            <p>Dependiendo del tipo de sala y horario, algunas reservas pueden tener costos.
-                Puedes consultar los precios en la sección de tarifas antes de confirmar tu reserva.</p>
-        </details>
-
-        <details>
-            <summary>¿Puedo cancelar o modificar una reserva?</summary>
-            <p>Sí, puedes cancelar o modificar tu reserva desde tu perfil en la sección "Mis reservas".
-                Ten en cuenta que algunas modificaciones pueden estar sujetas a políticas de cancelación.</p>
-        </details>
-
-        <details>
-            <summary>¿Las salas tienen equipamiento disponible?</summary>
-            <p>Sí, cada sala cuenta con un equipamiento básico.
-                Puedes consultar los detalles del equipamiento de cada sala en la descripción antes de hacer la reserva.
-            </p>
-        </details>
-
-        <details>
-            <summary>¿Qué sucede si llego tarde a mi reserva?</summary>
-            <p>El tiempo de uso de la sala comienza en el horario reservado.
-                Si llegas tarde, no se extenderá el tiempo de uso, y podría aplicarse una penalización si la ausencia es
-                total.</p>
-        </details>
-
-        <details>
-            <summary>¿Puedo reservar varias salas al mismo tiempo?</summary>
-            <p>Dependiendo de la disponibilidad y las políticas del sistema, es posible realizar múltiples reservas.
-                Verifica en la plataforma las restricciones aplicables.</p>
-        </details>
-
-        <details>
-            <summary>¿Se pueden hacer reservas recurrentes?</summary>
-            <p>Sí, puedes configurar una reserva recurrente si necesitas la sala con regularidad.
-                Consulta la opción "Reservas recurrentes" al realizar la reserva.</p>
-        </details>
+                    <details class="mb-1 bg-gray-50 p-4 rounded-lg shadow-sm cursor-pointer open:border-t-4 open:border-[#990000]">
+                        <summary>¿Hay costos asociados a la reserva de salas?</summary>
+                        <p class="mt-5">Dependiendo del tipo de sala y horario, algunas reservas pueden tener costos.
+                            Puedes consultar los precios en la sección de tarifas antes de confirmar tu reserva.</p>
+                    </details>
+               
+                    <details class="mb-1 bg-gray-50 p-4 rounded-lg shadow-sm cursor-pointer open:border-t-4 open:border-[#990000]">
+                        <summary>¿Puedo cancelar o modificar una reserva?</summary>
+                        <p class="mt-5">Sí, puedes cancelar o modificar tu reserva desde tu perfil en la sección "Mis reservas".
+                            Ten en cuenta que algunas modificaciones pueden estar sujetas a políticas de cancelación.</p>
+                    </details>
+            </div>
+            <div class="flex flex-col gap-2">
+                    <details class="mb-1 bg-gray-50 p-4 rounded-lg shadow-sm cursor-pointer open:border-t-4 open:border-[#990000]">
+                        <summary>¿Las salas tienen equipamiento disponible?</summary>
+                        <p class="mt-5">Sí, cada sala cuenta con un equipamiento básico.
+                            Puedes consultar los detalles del equipamiento de cada sala en la descripción antes de hacer la reserva.
+                        </p>
+                    </details>
+                
+                    <details class="mb-1 bg-gray-50 p-4 rounded-lg shadow-sm cursor-pointer open:border-t-4 open:border-[#990000]">
+                        <summary>¿Qué sucede si llego tarde a mi reserva?</summary>
+                        <p class="mt-5">El tiempo de uso de la sala comienza en el horario reservado.
+                            Si llegas tarde, no se extenderá el tiempo de uso, y podría aplicarse una penalización si la ausencia es
+                            total.</p>
+                    </details>
+                
+                    <details class="mb-1 bg-gray-50 p-4 rounded-lg shadow-sm cursor-pointer open:border-t-4 open:border-[#990000]">
+                        <summary>¿Puedo reservar varias salas al mismo tiempo?</summary>
+                        <p class="mt-5">Dependiendo de la disponibilidad y las políticas del sistema, es posible realizar múltiples reservas.
+                            Verifica en la plataforma las restricciones aplicables.</p>
+                    </details>
+                
+                    <details class="mb-1 bg-gray-50 p-4 rounded-lg shadow-sm cursor-pointer open:border-t-4 open:border-[#990000]">
+                        <summary>¿Se pueden hacer reservas recurrentes?</summary>
+                        <p class="mt-5">Sí, puedes configurar una reserva recurrente si necesitas la sala con regularidad.
+                            Consulta la opción "Reservas recurrentes" al realizar la reserva.</p>
+                    </details>
+            </div>
+        </div>
     </div>
     </section>
 
