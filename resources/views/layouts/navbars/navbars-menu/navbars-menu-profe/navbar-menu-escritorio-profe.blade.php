52c439c240eb503ce8b687602f0a9f4023337c6f
VictorMedac <vbj0003@alu.medac.es>
2025-05-26 13:35:19 +0200

    ruta profe

--- resources/views/layouts/navbars/navbars-menu/navbars-menu-profe/navbar-menu-escritorio-profe.blade.php
+++ resources/views/layouts/navbars/navbars-menu/navbars-menu-profe/navbar-menu-escritorio-profe.blade.php
@@ -7,7 +7,7 @@
     </a>
     <span class="text-[--color-primario] font-semibold">|</span>
     <a href="{{ url('gestion-salas') }}"
-        class="relative px-4 py-1.5 text-sm text-[--color-secundario] transition hover:bg-[--color-eventos] rounded-md duration-300 ease-in-out group {{ Request::is('gestion-salas') ? 'bg-[--color-eventos]' : '' }}">
+        class="relative px-4 py-1.5 text-sm text-[--color-secundario] transition hover:bg-[--color-eventos] rounded-md duration-300 ease-in-out group {{ request()->routeIs('gestion-salas', 'detalle-espacio') ? 'bg-[--color-eventos]' : '' }}">
         Gestión Salas
         <span class="absolute bottom-0 left-1/2 -translate-x-1/2 w-0 h-[3px] bg-[--color-primario] rounded-full transition-all duration-300 ease-in-out group-hover:w-3/4"></span>
     </a>
