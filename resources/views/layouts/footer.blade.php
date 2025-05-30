378a75a93032e6983870e352a00eb21d95b82304
eleenadm2 <edm0005@alu.medac.es>
2025-05-30 12:33:52 +0200

    fondo inicio

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="w-full relative mt-12 py-6">
+<footer class="w-full relative py-6">
     <div class="contenedor-contacto-redes-footer flex justify-between items-center h-full">
         <!-- Contenedor izquierdo footer -->
         <div class="contenedor-redes space-y-3 ml-4">
